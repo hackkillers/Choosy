1cc5a9de825a7f91c1349f17080c7d41dee49b05
Ana <anaprimawaty@gmail.com>
2015-06-25 13:06:52 +0800

    Added validations and tests for password

--- choosy_web/test/models/user_test.rb
+++ choosy_web/test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 	def setup
-		@user = User.new(name: "Example User", email: "user@example.com")
+		@user = User.new(name: "Example User", email: "user@example.com",
+										 password: "foobar", password_confirmation: "foobar")
 	end
 
 	test "should be valid" do
@@ -45,5 +46,11 @@ class UserTest < ActiveSupport::TestCase
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
   end
 end
