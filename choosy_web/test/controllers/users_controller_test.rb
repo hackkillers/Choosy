694de78a9b0bbc5a638ca185596c1322fb9cfe66
Ana <anaprimawaty@gmail.com>
2015-06-28 23:45:47 +0800

    Ensured only a signed in user can edit and update only  his/her details

--- choosy_web/test/controllers/users_controller_test.rb
+++ choosy_web/test/controllers/users_controller_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+    @user = users(:yellow)
+    @other_user = users(:blue)
+  end
+
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to signin_path
+  end
+
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to signin_path
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
